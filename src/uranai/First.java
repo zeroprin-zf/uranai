36a6dd2565ddb1ac032129a4b7fc36e357a21429
zeroz <zeroz@192.168.11.5>
2024-03-13 05:53:58 +0900

    二回目のコミット

--- src/uranai/First.java
+++ src/uranai/First.java
@@ -5,6 +5,22 @@ public class First {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(5) + 1;
+		switch (fortune) {
+		case 1:
+		case 2:
+			System.out.println("いいね!");
+			break;
+		
+		case 3:
+			System.out.println("普通です");
+			break;
+			
+		case 4:
+		case 5:
+			System.out.println("うーん");
+		}
 	}
 
 }
